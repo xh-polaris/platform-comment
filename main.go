106d0e5a37dd27f6706e03e1c8d56c2158cd0291
fuqihao.foliet <fuqihao.foliet@bytedance.com>
2023-10-07 15:21:13 +0800

    feat: log

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	"github.com/kitex-contrib/obs-opentelemetry/tracing"
+	"github.com/xh-polaris/gopkg/kitex/middleware"
 	logx "github.com/xh-polaris/gopkg/util/log"
 	"github.com/xh-polaris/service-idl-gen-go/kitex_gen/platform/comment/commentservice"
 
@@ -29,6 +30,7 @@ func main() {
 		server.WithServiceAddr(addr),
 		server.WithSuite(tracing.NewServerSuite()),
 		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: s.Name}),
+		server.WithMiddleware(middleware.LogMiddleware(s.Name)),
 	)
 
 	err = svr.Run()
