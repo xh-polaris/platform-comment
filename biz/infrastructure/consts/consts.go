fa64cd6cebc1d71d5852f2456a5215a796b61d20
wxl <1715142548@qq.com>
2023-10-15 16:33:45 +0800

    feat: 增加type的map

--- biz/infrastructure/consts/consts.go
+++ biz/infrastructure/consts/consts.go
@@ -1,1 +1,18 @@
 package consts
+
+import "github.com/xh-polaris/service-idl-gen-go/kitex_gen/platform/comment"
+
+var (
+	MapCommentTypeString = map[comment.CommentType]string{
+		comment.CommentType_CommentType_Unknown: "unknown",
+		comment.CommentType_CommentType_Comment: "comment",
+		comment.CommentType_CommentType_Post:    "post",
+		comment.CommentType_CommentType_Moment:  "moment",
+	}
+	MapStringCommentType = map[string]comment.CommentType{
+		"unknown": comment.CommentType_CommentType_Unknown,
+		"comment": comment.CommentType_CommentType_Comment,
+		"post":    comment.CommentType_CommentType_Post,
+		"moment":  comment.CommentType_CommentType_Moment,
+	}
+)
